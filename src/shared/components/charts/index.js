e93d243b50174c946575024afe0578748e4422fc
AartiNathani <144878986+AartiNathani@users.noreply.github.com>
2025-09-20 19:43:47 +0530

    Done with Institute Dashboard UI

--- src/shared/components/charts/index.js
+++ src/shared/components/charts/index.js
@@ -1,2 +1,3 @@
 export { default as DoubleCircleChart } from './DoubleCircleChart'
 export { default as TradesPieChart } from './TradesPieChart'
+export { default as CircularProgressChart } from './ProgressChart'
