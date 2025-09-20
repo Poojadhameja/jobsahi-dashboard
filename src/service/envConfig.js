0aff93522e7a9e9b25a358ba9de4cbea74f86b4c
Deepak Kumar Thakur <85386465+deepakmhthakur@users.noreply.github.com>
2025-09-20 15:22:57 +0530

    Check-in Auth API Integration Login & Registration in Front-end
    Check-in Auth API Integration Login & Registration in Front-end

---
+++ src/service/envConfig.js
@@ -0,0 +1,4 @@
+export const env = {
+    apiHost: "http://localhost/JOBSAHI-API/api",
+    frontUrl: "http://localhost:5173",
+};
---
+++ src/service/serviceUrl.js
@@ -0,0 +1,6 @@
+const service = {
+    signin: '/auth/login.php',
+    signup: '/user/create_user.php'
+}
+
+export default service;
