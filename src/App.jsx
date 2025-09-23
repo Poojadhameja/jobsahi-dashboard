8cfafbabe29a44ca49fca1a24c23c4383c647a1d
Deepak Kumar Thakur <85386465+deepakmhthakur@users.noreply.github.com>
2025-09-24 01:35:36 +0530

    Check-in Role Based Routing
    Check-in Role Based Routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './shared/auth/Login.jsx'
 import CreateAccount from './shared/auth/CreateAccount.jsx'
+import RoleRoute from "./RoleRoute.jsx";
 
 const AdminRoutes = lazy(() => import('./features/admin/routes.jsx'))
 const RecruiterRoutes = lazy(() => import('./features/recruiter/routes.jsx'))
 
 export default function App() {
+
   return (
     <Router>
       <Suspense fallback={<div className="p-8">Loading...</div>}>
@@ -14,8 +16,12 @@ export default function App() {
           <Route path="/" element={<Navigate to="/login" replace />} />
           <Route path="/login" element={<Login />} />
           <Route path="/create-account" element={<CreateAccount />} />
-          <Route path="/admin/*" element={<AdminRoutes />} />
-          <Route path="/recruiter/*" element={<RecruiterRoutes />} />
+          <Route path="/admin/*" element={ <RoleRoute allowedRoles={["admin"]}>
+              <AdminRoutes />
+            </RoleRoute> } />
+          <Route path="/recruiter/*" element={<RoleRoute allowedRoles={["recruiter"]}>
+              <RecruiterRoutes />
+            </RoleRoute>} />
           <Route path="*" element={<div className="p-8">404 — Not Found</div>} />
         </Routes>
       </Suspense>
