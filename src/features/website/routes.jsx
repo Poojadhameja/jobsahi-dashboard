251a5a8f33118a67f90a877fba1bb12f76feafb5
Pooja Dhameja <poojadhameja36@gmail.com>
2025-10-01 21:45:22 +0530

    website routing

--- src/features/website/routes.jsx
+++ src/features/website/routes.jsx
@@ -19,7 +19,7 @@ export default function WebsiteRoutes() {
     <Routes>
       <Route index element={<Home />} />
       <Route path="/about" element={<About />} />
-      <Route path="/find-jobs" element={<FindJob />} />
+      <Route path="/find-job" element={<FindJob />} />
       <Route path="/courses" element={<Courses />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/skill-assessment" element={<SkillAssesment />} />
